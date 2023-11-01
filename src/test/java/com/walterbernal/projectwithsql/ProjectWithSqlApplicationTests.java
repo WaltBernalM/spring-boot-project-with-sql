4db90763735a5633eff56f1f229dc06802caacb3
Walter Bernal <wltrbm@gmail.com>
2023-11-01 13:02:00 -0700

    first commit

---
+++ src/test/java/com/walterbernal/projectwithsql/ProjectWithSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walterbernal.projectwithsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
