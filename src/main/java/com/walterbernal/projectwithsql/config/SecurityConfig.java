badf8b492c2c67354a1180aaf76b1517f65dc14c
Walter Bernal <wltrbm@gmail.com>
2023-11-03 11:29:53 -0700

    craetion of templates and config of mvc

---
+++ src/main/java/com/walterbernal/projectwithsql/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.walterbernal.projectwithsql.config;
+
+// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+// @EnableWebSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+  
+}
